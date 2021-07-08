7d249c4e12cdb1192d7a2764d451e87b72e2620c
AuraCamilaPicoAraque <auroraprogamer@gmail.com>
2021-07-07 21:33:59 -0500

    repaso de las variables 07/07/21

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+nombreCompletoDelProfesor ='Aura Camila Pico Araque';
+miEdad = 13;
+colegio ='Balbino Garcia';
+altura  = 1.60;
+Ciudad ='Piedecuesta'
+soyUnProfesor = false ;
+Deporte ='basquetbol';
+comidaFavorita ='pizza y hamburguesa';
+
+
+console.log(nombreCompletoDelProfesor, typeof (nombreCompletoDelProfesor))
+console.log(miEdad, typeof (miEdad))
+console.log(colegio, typeof (colegio))
+console.log(altura, typeof (altura))
+console.log(Ciudad, typeof (Ciudad))
+console.log(soyUnProfesor, typeof (soyUnProfesor))
+console.log(Deporte, typeof (Deporte))
+console.log(comidaFavorita, typeof (comidaFavorita))
